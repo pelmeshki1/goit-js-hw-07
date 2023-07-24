46439d371bc85d32f57e53fabdc4558ba80bc328
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-24 19:36:37 +0300

    update 2.2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,6 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
