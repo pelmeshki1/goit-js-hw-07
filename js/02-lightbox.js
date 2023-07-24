79c233d8fedc6a89da440f75edcf422b270b8925
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-24 20:25:00 +0300

    update 6

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,3 +4,21 @@ import { galleryItems } from "./gallery-items.js";
 const galleryContainerEl = document.querySelector(".gallery");
 const imagesMarkup = createItemsMarkup(galleryItems);
 galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+      <li>
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      </li>
+     
+    </ul>`;
+    })
+    .join("");
+}
