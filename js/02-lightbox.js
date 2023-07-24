4230d89ab68f34d4424bdb44cf130d201bccbb1f
pelmeshki1 <108807142+pelmeshki1@users.noreply.github.com>
2023-07-24 20:21:21 +0300

    update 5

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
